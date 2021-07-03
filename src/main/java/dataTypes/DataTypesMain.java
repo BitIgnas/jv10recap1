6ab7ca7c712a4adfb24ac24536a48fab6ec9a06c
IgnatLT79 <Ignotas99@gmail.com>
2021-07-03 11:38:07 +0300

    added datatypes

---
+++ src/main/java/dataTypes/DataTypesMain.java
@@ -0,0 +1,24 @@
+package dataTypes;
+
+public class DataTypesMain {
+
+    public static void main(String[] args) {
+        byte sd = 127;
+        short streetNumber = 5;
+        int age = 27;
+        long salary = 2780;
+
+        float height = 1.84f;
+        double weight = 123.02;
+
+        boolean isValid = true;
+        boolean isNot = false;
+
+        char letter = 'A';
+
+        int[] arrayOfInt = new int[]{3, 5, 6};
+        String text = "text";
+
+
+    }
+}
